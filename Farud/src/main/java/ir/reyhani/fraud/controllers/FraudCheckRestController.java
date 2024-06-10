f433b9da5b0d32eefc2ab7f8fa313b94afe06fff
Ahmadreza <ahmadrezareyhani@aryehco.com>
2024-06-10 12:39:29 +0330

    + add Log to isFraudster API

--- Farud/src/main/java/ir/reyhani/fraud/controllers/FraudCheckRestController.java
+++ Farud/src/main/java/ir/reyhani/fraud/controllers/FraudCheckRestController.java
@@ -2,6 +2,7 @@ package ir.reyhani.fraud.controllers;
 
 import ir.reyhani.fraud.models.dto.FraudCheckResponse;
 import ir.reyhani.fraud.services.FraudCheckService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,12 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 6/10/2024 - 11:21 AM
  */
 
+@Slf4j
 @RestController
 @RequestMapping("api/v1/fraud-check")
 public record FraudCheckRestController(FraudCheckService fraudCheckService) {
 
     @GetMapping(path = "{customerId}")
     public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId) {
+        log.info("fraud check request for customerId {}", customerId);
         boolean fraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerId);
         return new FraudCheckResponse(fraudulentCustomer);
     }
